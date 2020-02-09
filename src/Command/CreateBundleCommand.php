d10ffa30f225091b33416c5fea0642e99dcdf741
msalsas <manolez@gmail.com>
2020-02-09 12:54:05 +0100

    Echo composer install output

--- src/Command/CreateBundleCommand.php
+++ src/Command/CreateBundleCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
@@ -227,6 +228,11 @@ class CreateBundleCommand extends Command
 
         $process = new Process(['composer install']);
         $process->run();
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        echo $process->getOutput();
 
         $event = $stopwatch->stop('create-bundle-command');
         if ($output->isVerbose()) {
